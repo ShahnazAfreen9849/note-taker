ffc2a4649093419cfe455d2d73d650a89f426d23
Shanzzz <shahnazafreen9849@gmail.com>
2023-05-03 23:40:33 -0400

    corrected typo

--- Develop/routes/index.js
+++ Develop/routes/index.js
@@ -5,7 +5,7 @@ const notesRouter = require('./notes');
 
 const app = express();
 //http://localhost:3001/api/notes
-app.use('notes', notesRouter);
+app.use('/notes', notesRouter);
 
 
 module.exports = app;
--- Develop/routes/notes.js
+++ Develop/routes/notes.js
@@ -1,33 +1,33 @@
-const notes = require('express'). Router();
-const {v4: uuidv4} =require ('uuid');
+const notes = require('express').Router();
+const { v4: uuidv4 } = require('uuid');
 const {
-    readFromFIle,
-    readAndAppend,
-    writeToFIle,
-}= require('../helpers/fsUtils');
+  readFromFile,
+  readAndAppend,
+  writeToFile,
+} = require('../helpers/fsUtils');
 //http://localhost:3001/api/notes/
 notes.get('/', (req, res) => {
-    readFromFIle('./db/db.json').then((data) => res.json(JSON.parse(data)));
+  readFromFIle('./db/db.json').then((data) => res.json(JSON.parse(data)));
 });
 
 notes.post('/', (req, res) => {
-    console.log(req.body);
-  
-    const { title,text} = req.body;
-  
-    if (req.body) {
-      const newNotes = {
-        title,
-        text,
-       id: uuidv4(),
-      };
-  
-      const parseData =readAndAppend(newNotes, './db/db.json');
+  console.log(req.body);
 
-      res.json(parseData);
-    } else {
-      res.error('Error in adding notes');
-    }
-  });
-  
-  module.exports = notes;
+  const { title, text } = req.body;
+
+  if (req.body) {
+    const newNotes = {
+      title,
+      text,
+      id: uuidv4(),
+    };
+
+    const parseData = readAndAppend(newNotes, './db/db.json');
+
+    res.json(parseData);
+  } else {
+    res.error('Error in adding notes');
+  }
+});
+
+module.exports = notes;
